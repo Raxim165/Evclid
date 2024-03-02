239c7f8e7a330d2aba0f7e26c1778b770e7ae5d8
Raxim <rahtag165@gmail.com>
2024-03-02 16:59:38 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// const age = 20;
+
+
+alert('fff')
+const result = confirm('вам есть 18?')
+
+
+// console.log(result)
+// // const answer = prompt('сколько вам лет', '100')
+// const answer = +prompt('сколько вам лет', '100')//+ меняет строку на число. такой плюс называется онарным.
+// console.log(answer)
+
+// if (age > 18 && age < 25) {
+//   console.log('ты совершеннолетный')
+// }
+// if (age < 17) {
+//   console.log('ты несовершеннолетный')
+// }
+// if (age > 30) {
+//   console.log('ты взрослый')
+// }
